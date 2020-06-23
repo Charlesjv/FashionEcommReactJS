a5bbcfa19f4532af23e692ebdac6a1b18831ce65
CharlesJV <charles764jaison@gmail.com>
2020-06-23 11:02:52 -0400

    Added Cart dropdown components to our application

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,6 +6,7 @@ import './cart-dropdown.styles.scss';
 const CartDropdown = ()=>(
     <div className = 'cart-dropdown'>
         <div className = 'cart-items'></div>
+        <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
