007076bfb42343a058f44d33f1927f2f8351247c
CharlesJV <charles764jaison@gmail.com>
2020-06-27 03:09:26 -0400

    Added Collection component and route to each collection

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,15 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview-component';
+import {Route} from 'react-router-dom';
+import CollectionPage from '../collection/collection.component'
 
-class Shop extends React.Component{
-    constructor(props){
-        super(props);
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+const ShopPage =({match}) => {
+        console.log(match);
+        return (
+        <div className = 'shop-page'>
+            <Route exact path = {`${match.path}`} component = {CollectionsOverview}></Route>
+            <Route path = {`${match.path}/:collectionId`} component ={CollectionPage}></Route>
+        </div>   );
+};
 
-        this.state = {
-            collections: SHOP_DATA 
-        }
-    }
-
-    render(){
-        const {collections} = this.state;
-
-        return (<div className = 'shop-page'>
-
-        {collections
-        .map(({id,...collectionProps})=>(
-            <CollectionPreview key = {id}{...collectionProps}></CollectionPreview>
-        ))}
-        </div>)
-
-    }
-}
-
-export default Shop;
+export default ShopPage;
